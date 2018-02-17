a45f049365fc7ed024f258dcdc05f38910ffc73d
=Bill Withers <wither7007@gmail.com>
2018-02-17 00:21:27 -0600

    initial commit

---
+++ hist.js
@@ -0,0 +1,14 @@
+fs = require('fs')
+let uniq = a => [...new Set(a)];
+fs.readFile('l', function(err, data){
+    if (err) {
+        return console.error(error)
+    }
+
+		re = /\s?\d+\s+/g
+    let nData=data.toString().replace(re,'').trim().split('\n')
+		uData=(uniq(nData))
+//		nData=mData.replace(re,'').trim().split('\n').trim()
+		uData.forEach(a=>console.log(a))
+   // console.log(uniq(nData))
+})
