c9ba74e8bed93b09e2831791ac1143994c3d849c
James Steffes <wither7007@gmail.com>
2018-04-10 11:45:49 -0500

    removed comments from bottom

--- scrap/scrape.js
+++ scrap/scrape.js
@@ -1,18 +1,19 @@
-const axios=require('axios')
+const axios = require('axios')
 const jsdom = require('jsdom')
-const {JSDOM} = jsdom
+const { JSDOM } = jsdom
 var pretty = require('pretty')
-var fs=require('fs')
+var fs = require('fs')
+const murl = "https://example.com"
 
-axios.get('https://umn.edu')
-    .then(response=>console.log(pretty(response.data)))
-// var text=fs.readFileSync('wal','utf8')
-// const dom = new JSDOM(text)
-// console.log(pretty(dom.window.document.querySelector('script').innerHTML))
-//console.log(pretty(text))
-// let username = 'wither7007'
-// axios.get('https://api.github.com/users/' + username)
-//   .then(function(response){
-//     console.log(response.data); // ex.: { user: 'Your User'}
-//     console.log(response.status); // ex.: 200
-//   });  
+axios.get(murl)
+    .then(function (response) {
+        // console.log(pretty(response.data))
+        let myText=new JSDOM(response.data)
+       n(myText) 
+
+        // console.log(myText.window.document.querySelector("p").innerHTML)
+    }
+    )
+n=(l)=>{ }
+console.log('done')
+
